3aa7bfeecb594c7ad54cb9e56b43672f98034e79
Anton Katunin <antulik@gmail.com>
2013-04-06 16:14:00 +1000

    changed log to debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ StoryReddit::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  config.log_level = :debug
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
